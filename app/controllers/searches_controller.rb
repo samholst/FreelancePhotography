3cc97087b07d0579ce1a1e54f4d42e4fdc750fef
Sam Holst <sawohol@gmail.com>
2017-09-20 14:27:35 -0600

    updated controller query

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,15 @@
 class SearchesController < ApplicationController
   def index
-    @results = User.where("LOWER(complete_address) LIKE '%#{params[:location].downcase}%'")
+    @results = fetch_results params[:location]
     @search_city_state = params[:location]
   end
 
   private
     def fetch_params
       params.permit(:location)
+    end
+
+    def fetch_results param
+      User.where("LOWER(complete_address) LIKE '%#{param.downcase}%'")
     end
 end
