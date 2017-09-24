48c3f06f60a6eb2a71ebfbe354c94bacdb64b1aa
Sam Holst <sawohol@gmail.com>
2017-09-24 16:46:47 -0600

    updated slim for index

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @results = fetch_results params[:location]
+    @lat_langs = @results.pluck(:lat_lang)
     @search_city_state = params[:location]
   end
 
@@ -12,4 +13,8 @@ class SearchesController < ApplicationController
     def fetch_results param
       User.where("LOWER(complete_address) LIKE '%#{param.downcase}%'").order("created_at DESC")
     end
+
+    # def fetch_lat_langs param
+    #   User.where("LOWER(complete_address) LIKE '%#{param.downcase}%'").order("created_at DESC").pluck(:lat_lang)
+    # end
 end
