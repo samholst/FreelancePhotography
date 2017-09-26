8209e6fd0d54610e796e9f473769f28ae72330c2
Sam Holst <sawohol@gmail.com>
2017-09-26 15:50:50 -0600

    updated geocoder and css

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   def set_complete_address
     self.city = self.city.strip
     self.state = self.state.strip
-    self.complete_address = "#{self.address} #{self.city.capitalize}, #{self.state.upcase}, Unites States}"
+    self.complete_address = "#{self.address} #{self.city.capitalize}, #{self.state.upcase}, United States"
     location = Geocoder.coordinates(self.complete_address)
     self.lat_lang = "#{location[0]},#{location[1]}"
   end
