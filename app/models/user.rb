37e14ee333c838b3748fe3324036a53b543029b4
Sam Holst <sawohol@gmail.com>
2017-09-26 15:25:30 -0600

    updated views and google autocomplete

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   def set_complete_address
     self.city = self.city.strip
     self.state = self.state.strip
-    self.complete_address = "#{self.address} #{self.city.capitalize}, #{self.state.upcase} #{self.postal_code}"
+    self.complete_address = "#{self.address} #{self.city.capitalize}, #{self.state.upcase}, Unites States}"
     location = Geocoder.coordinates(self.complete_address)
     self.lat_lang = "#{location[0]},#{location[1]}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'pages/contact'
   # resources :profiles
   get 'profile/:id', to: 'profiles#index', as: 'profile'
   get 'search/:location', to: 'searches#index'
